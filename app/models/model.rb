7cc5dea2fc117f072e8ae3c7ccfea39b5e9e4e82
joshua richards <jermain119@gmail.com>
2019-11-23 19:27:45 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,16 +1,27 @@
 class Dog
+<<<<<<< HEAD
  @@dogs = []
+=======
+ 
+>>>>>>> 9d844a0a55dddac11a73bd634d2e21b68783cba2
 attr_accessor :name, :breed, :age
 
 def initialize(name,breed,age)
   @age = age
   @name= name
   @breed = breed
+<<<<<<< HEAD
   @@dogs << self
 end
 
 def self.all
 @@dogs
+=======
+end
+
+def self.all
+
+>>>>>>> 9d844a0a55dddac11a73bd634d2e21b68783cba2
 end 
 
 end 
